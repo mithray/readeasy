a1f9ed7b7e1040ca1495b55182eb4fe0042eddb6
abc123 <abc123@gmail.com>
2019-08-24 16:26:49 +1200

    update version

--- test.js
+++ test.js
@@ -1,7 +1,14 @@
 const readeasy = require('./index.js')
+const fs = require('fs')
 
-let text = "Always use shorter words where possible."
-let readability = readeasy(text)
+let text1 = "Always use shorter words where possible."
+let text2 = `First Paragraph. This is to test the readability of text. Right now it only works for entire blocks of text. Soon I will fix it to divide by sentences so that it can provide hints about specific sentences that need might need simplifying.
 
-console.log(readability)
+This is the text to a second paragraph. This is to test the readability of text. Right now it only works for entire blocks of text. Soon I will fix it to divide by sentences so that it can provide hints about specific sentences that need might need simplifying.`
+
+fs.writeFile('./testfile.json', JSON.stringify(readeasy(text2)), 'utf8',function (err) {
+    if (err) {
+        return console.log(err);
+    }
+})
 
